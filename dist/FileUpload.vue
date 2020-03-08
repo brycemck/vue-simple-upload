65d50730b4bc8583598037ffb339bd8fb690e2ae
Bryce McKenney <brycemckenney@gmail.com>
2020-03-07 18:01:32 -0800

    added id attr to input

--- dist/FileUpload.vue
+++ dist/FileUpload.vue
@@ -1,5 +1,5 @@
 <template>
-  <input type="file" name="fileUpload" @change="onFileChange" :id="id">
+  <input type="file" name="fileUpload" @change="onFileChange" id="{{id}}">
 </template>
 
 <script type="text/babel">
